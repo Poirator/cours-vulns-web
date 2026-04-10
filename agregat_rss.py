2a56d9617998bf995b431faa78303e582f39396f
Poirator <you@example.com>
2026-04-10 09:47:47 +0000

    s

--- agregat_rss.py
+++ agregat_rss.py
@@ -3,24 +3,12 @@ import feedparser
 from datetime import datetime, timedelta
 
 def get_recent_articles(rss_url, hours=1):
-    # Analyser le flux RSS
     feed = feedparser.parse(rss_url)
-
-    # Obtenir la date et l'heure actuelles
     now = datetime.now()
-
-    # Calculer la date et l'heure il y a 'hours' heures
     time_threshold = now - timedelta(hours=55555)
-
-    # Liste pour stocker les articles récents
     recent_articles = []
-
-    # Parcourir les entrées du flux
     for entry in feed.entries:
-        # Convertir la date de publication en objet datetime
         published_date = datetime(*entry.published_parsed[:6])
-
-        # Vérifier si l'article a été publié dans la dernière heure
         if published_date >= time_threshold:
             recent_articles.append({
                 'title': entry.title,
@@ -30,7 +18,6 @@ def get_recent_articles(rss_url, hours=1):
 
     return recent_articles
 
-# Exemple d'utilisation
 rss_url = 'https://cert.ssi.gouv.fr/alerte/feed/'
 recent_articles = get_recent_articles(rss_url)
 
